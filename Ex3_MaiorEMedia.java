83f74211f79363b194dc22b523e70ceb3f40f0e5
Gabriel-Martinelli <gabrielmartinelli35@gmail.com>
2022-11-07 16:32:04 -0300

    Exercício 3

---
+++ Ex3_MaiorEMedia.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ex3_MaiorEMedia {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int numero = 0;
+        int soma = 0;
+        int maior = 0;
+
+        int i = 0;
+        while(i < 5){
+            System.out.println("Digite um número: ");
+            numero = scan.nextInt();
+            soma = soma + numero;
+
+            if(numero > maior) maior = numero;
+
+            i = i + 1;
+
+        }
+
+        float media = soma / 5F;
+
+        String mensagem = String.format("O maior número é %d, e a média é %f", maior, media);
+        System.out.println(mensagem);
+    }
+}
