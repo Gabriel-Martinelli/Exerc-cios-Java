a9267c786f5a9652a74ba716317321609cb880fa
Gabriel-Martinelli <gabrielmartinelli35@gmail.com>
2022-11-08 12:51:30 -0300

    Exercício 6

---
+++ Ex6_Fatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        int mult = 1;
+
+        System.out.println("Digite um número: ");
+        num = scan.nextInt();
+
+        while(num > 1){
+            mult = mult * num;
+            num--;
+
+                if(num == 1){
+                System.out.println("O fatorial é: " + mult);
+                }
+            }
+        }
+    }
+
